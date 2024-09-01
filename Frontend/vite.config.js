7cd5c7bc6f6f4b66ef4fef1d74d815f9f2f85874
Aditya-Patel24 <adityasp243@gmail.com>
2024-09-02 00:03:43 +0530

    Refactor API endpoints to use relative paths

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 10000, // Ensure this port matches Render’s expected port
+    host: true   // Binds to all network interfaces
+  },
   plugins: [react()],
 })

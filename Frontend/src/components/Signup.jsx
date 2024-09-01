7e5973b50eaa6ed016d64150683e5d0fe2b2f756
Aditya-Patel24 <adityasp243@gmail.com>
2024-09-02 00:09:25 +0530

    Refactor API endpoints to use HTTPS for book API calls

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("/user/signup", userInfo)
+      .post("https://book-r2wm.onrender.com//user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {

206cc714091189a34df961e41c2d0dfd02d75797
Aditya-Patel24 <adityasp243@gmail.com>
2024-09-02 00:42:42 +0530

    Refactor API endpoints to use HTTPS for book API calls

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,10 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://book-r2wm.onrender.com'
+  }));
+  
 app.use(express.json());
 
 dotenv.config();
